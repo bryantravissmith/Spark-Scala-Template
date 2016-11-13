06ca9a4fef7ae270eb2817e4967cd4691267d785
Bryan Travis Smith <bryantravissmith@gmail.com>
2016-11-13 09:52:04 -0800

    added local

--- src/main/scala/org/apache/spark/examples/SparkPi.scala
+++ src/main/scala/org/apache/spark/examples/SparkPi.scala
@@ -27,10 +27,11 @@ object SparkPi {
   def main(args: Array[String]) {
     val spark = SparkSession
       .builder
+      .master("local[*]") //Added from originals for local run in IntelliJ
       .appName("Spark Pi")
       .getOrCreate()
     val slices = if (args.length > 0) args(0).toInt else 2
-    val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
+    val n = math.min(1000000L * slices, Int.MaxValue).toInt // avoid overflow
     val count = spark.sparkContext.parallelize(1 until n, slices).map { i =>
       val x = random * 2 - 1
       val y = random * 2 - 1
